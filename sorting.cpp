b52e283a9fa87728cf19ce2ef41a24761d86583a
Optifun <frdima19@gmail.com>
2019-10-17 17:14:26 +0700

    Улучшение сортировки чет-нечет

--- sorting.cpp
+++ sorting.cpp
@@ -50,10 +50,17 @@ T* BubbleEven(T* arr, long length=-1)
 {
 	T* narr = new T[length];
 	memcpy(narr, arr, sizeof(T)*length);
-	for (long i = 0; i < length; i++)
+	bool sorted = false;
+	for (long i=0; !sorted; i++)
+	{
+		sorted = true;
 		for (long j = (i + 1) % 2; j < length - 1; j += 2)
 			if (narr[j] > narr[j + 1])
+			{
 				Swap(narr[j], narr[j + 1]);
+				sorted = false;
+			}
+	}
 	return narr;
 }
 
