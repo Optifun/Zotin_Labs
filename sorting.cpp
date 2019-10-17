07f3b7b214ce2f6e68250a3532bc67857eca1a35
Optifun <frdima19@gmail.com>
2019-10-17 17:07:40 +0700

    Файл с сортировками

---
+++ sorting.cpp
@@ -0,0 +1,64 @@
+#pragma once
+#include<omp.h>
+#include<Windows.h>
+
+template<class T>
+T Max(long count, T args, ...)
+{
+	T* arg = &args;
+	T _max = args;
+	for (; count; count--, arg++)
+	{
+		if (*arg > _max)
+			_max = *arg;
+	}
+	return _max;
+}
+
+template<class T>
+void Swap(T& one, T& two)
+{
+	T temp = one;
+	one = two;
+	two = temp;
+}
+
+template<class T>
+T* Bubble(T* arr, long length)
+{
+	bool sorted = false;
+	T* narr = new T[length];
+	memcpy(narr, arr, sizeof(T)*length);
+	while (!sorted)
+	{
+		sorted = true;
+		for (long i = 0; i < length-1; i++)
+		{
+			if (narr[i] > narr[i + 1])
+			{
+				Swap(narr[i], narr[i + 1]);
+				sorted = false;
+			}
+		}
+	}
+	return narr;
+
+}
+
+template<class T>
+T* BubbleEven(T* arr, long length=-1)
+{
+	T* narr = new T[length];
+	memcpy(narr, arr, sizeof(T)*length);
+	for (long i = 0; i < length; i++)
+		for (long j = (i + 1) % 2; j < length - 1; j += 2)
+			if (narr[j] > narr[j + 1])
+				Swap(narr[j], narr[j + 1]);
+	return narr;
+}
+
+template<class T>
+void BubbleEvenAsync(T*& arr, long &length)
+{
+
+}
