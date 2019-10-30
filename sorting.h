1a58b948d86eb92145a8b91bfe01f5618223cb76
Optifun <36756042+Optifun@users.noreply.github.com>
2019-10-30 19:46:24 +0700

    Ну как бы первая лаба готова (#6)
    + Комментарии
    + Функция проверки на упорядоченность
    + Функция заполнения случайными числами
    + Шаблонный компаратор
    + Два асинхронных метода фильтрации
    + Убрал мусор

--- sorting.h
+++ sorting.h
@@ -1,17 +1,70 @@
 ﻿#pragma once
 #include<omp.h>
-//#include<Windows.h>
-
-//template<typename Type>
+#include<queue>
 typedef bool(*IntComparer)(int, int);
 typedef bool(*DoubleComparer)(double, double);
-typedef int*(*SortingMethod)(int* arr, long length, IntComparer compare);
 
-const IntComparer Ascending = [](int one, int two)->bool { return one > two; }; //сортирует по возрастанию
-const IntComparer Descending = [](int one, int two)->bool { return one < two; }; //сортирует по убыванию
+typedef int*(*SortingInt)(int* arr, long length, IntComparer compare);//Алгоритм сортировки целых чисел
+typedef double*(*SortingDouble)(double* arr, long length, DoubleComparer compare);//Алгоритм сортировки чисел с плавающей точкой
 
+const IntComparer AscInt = [](int one, int two)->bool { return one > two; }; //сортирует по возрастанию
+const IntComparer DescInt = [](int one, int two)->bool { return one < two; }; //сортирует по убыванию
+const DoubleComparer AscDouble = [](double one, double two)->bool { return one > two; }; //сортирует по возрастанию
+const DoubleComparer DescDouble = [](double one, double two)->bool { return one < two; }; //сортирует по убыванию
 
+#define Random(a, b) (double)rand() / (RAND_MAX + 1)*((b)-(a)) + (a)
 
+//Не советую трогать, изучать. Лучше не стоит..
+namespace magic
+{
+template<class T>
+class Comparator
+{
+public:
+	bool operator()(const T& one, const T& two)
+	{
+		return ASC == order_ ? (two < one) : (one < two);
+	}
+	//по возрастанию
+	static const Comparator& Asc()
+	{
+		return asc_;
+	}
+	//по убыванию
+	static const Comparator& Desc()
+	{
+		return desc_;
+	}
+private:
+	enum Order_ { ASC, DESC } const order_;
+	Comparator(Order_ order) : order_(order) {}; // первичная инициализация
+	static Comparator asc_;
+	static Comparator desc_;
+};
+template<class T> Comparator<T> Comparator<T>::asc_(Comparator<T>::ASC); //инициализация
+template<class T> Comparator<T> Comparator<T>::desc_(Comparator<T>::DESC);
+}
+
+//Проверяет последовательность на упорядоченноть
+template<class T>
+bool isOrdered(T* arr, long length, IntComparer order)
+{
+	bool ord = true;
+	for (int i = 0; i < length - 1; i++)
+		if (order(arr[i], arr[i + 1]))
+			return false;
+	return true;
+}
+
+//Заполняет последовательность случайными числами
+template<class T>
+void Randomize(T *arr, long length)
+{
+	for (long i = 0; i < length; i++)
+		arr[i] = Random(-50, 50);
+}
+
+//Находит максимальный элемент в последовательности аргументов
 template<class T>
 T Max(long count, T args, ...)
 {
@@ -25,6 +78,7 @@ T Max(long count, T args, ...)
 	return _max;
 }
 
+//Обмен значениями
 template<class T>
 void Swap(T& one, T& two)
 {
@@ -33,8 +87,9 @@ void Swap(T& one, T& two)
 	two = temp;
 }
 
+//Сортировка обменами
 template<class T, typename TComparer>
-T* Bubble(T* arr, long length, TComparer copare)
+T* Bubble(T* arr, long length, TComparer compare)
 {
 	bool sorted = false;
 	T* narr = new T[length];
@@ -75,8 +130,9 @@ T* BubbleEven(T* arr, long length, TComparer compare)
 	return narr;
 }
 
+//Сортировка чет-нечет параллельная
 template<class T, typename TComparer>
-T* BubbleEvenAsync(T*& arr, long length, TComparer compare)
+T* BubbleEvenAsync(T* arr, long length, TComparer compare)
 {
 	T* narr = new T[length];
 	memcpy(narr, arr, sizeof(T)*length);
@@ -84,7 +140,7 @@ T* BubbleEvenAsync(T*& arr, long length, TComparer compare)
 	for (long i = 0; !sorted; i++)
 	{
 		sorted = true;
-		#pragma omp for shared(i, sorted, narr) 
+		#pragma omp parallel for shared(i, sorted, narr) 
 		for (long j = (i + 1) % 2; j < length - 1; j += 2)
 			if (compare(narr[j], narr[j + 1]))
 			{
@@ -95,6 +151,7 @@ T* BubbleEvenAsync(T*& arr, long length, TComparer compare)
 	return narr;
 }
 
+//Сортировка Шелла
 template<class T, typename TComparer>
 T* ShellSort(T *arr, long length, TComparer compare)
 {
@@ -111,6 +168,7 @@ T* ShellSort(T *arr, long length, TComparer compare)
 	return narr;
 }
 
+//Сортировка Шелла параллельная
 template<class T, typename TComparer>
 T* ShellSortAsync(T *arr, long length, TComparer compare)
 {
@@ -128,86 +186,130 @@ T* ShellSortAsync(T *arr, long length, TComparer compare)
 	return narr;
 }
 
-//template<class T>
-//void quickSortR(T*& arr, long length) {
-//	// На входе - массив a[], a[N] - его последний элемент.
-//	long i = 0, j = length - 1; 		// поставить указатели на исходные места
-//	T p;
-//	p = arr[length >> 1];		// центральный элемент
-//						// процедура разделения
-//	do {
-//		while (arr[i] < p) i++;
-//		while (arr[j] > p) j--;
-//
-//		if (i <= j) {
-//			Swap(arr[i], arr[j]);
-//			i++; j--;
-//		}
-//	} while (i <= j);
-//
-//	// рекурсивные вызовы, если есть, что сортировать 
-//	if (j > 0) quickSortR(arr, j);
-//	T* pointer = &arr[i];
-//	if (length > i) quickSortR(pointer, length - i);
-//}
-
-//template<class T>
-void quickSortR(int* a, long N) {
-	// На входе - массив a[], a[N] - его последний элемент.
-
-	long i = 0, j = N - 1; 		// поставить указатели на исходные места
-	int temp, p;
-
-	p = a[N >> 1];		// центральный элемент
-
-						// процедура разделения
+//Быстрая сортировка
+template<class T>
+void quickSortR(T* a, long N) {
+	long i = 0, j = N - 1;
+	T  p;
+	p = a[N >> 1];
 	do {
 		while (a[i] < p) i++;
 		while (a[j] > p) j--;
 
 		if (i <= j) {
-			temp = a[i]; a[i] = a[j]; a[j] = temp;
+			Swap(a[i], a[j]);
 			i++; j--;
 		}
 	} while (i < j);
-
-
-	// рекурсивные вызовы, если есть, что сортировать 
-	if (j > 0) quickSortR(a, j + 1);
-	if (N > i) quickSortR(a + i, N - i);
+	//if (j > 0) cout << "( 0 : " << j << " )";
+	//if (N > i) cout << "( " << i << " : " << N-1 << " )";
+	//cout << endl;
+	if (j > 0)
+		quickSortR(a, j + 1);
+	if (N > i)
+		quickSortR(a + i, N - i);
 }
 
-template<class T>
-T* quickSortAsync(T* arr, long length) {
-	// На входе - массив a[], a[N] - его последний элемент.
+//template<class T>
+int* quickSortAsync(int* arr, long length) {
+	int* narr = new int[length];
+	memcpy(narr, arr, sizeof(int)*length);
 
-	long i = 0, j = length - 1; 		// поставить указатели на исходные места
-	T* narr = new T[length];
-	memcpy(narr, arr, sizeof(T)*length);
-	T temp, p;
-	//p = arr[length >> 1];		// центральный элемент
-								// процедура разделения
-	for (int partLen = length; partLen > 1; partLen /= 2) // дроблю массив на подмассивы
-	{//parallel for
-		int count = length / partLen;
-		#pragma omp parallel for shared(narr) firstprivate(i, j, part, partLen)
+	// очередь границ интервалов
+	std::queue<int> iterators = std::queue<int>();
+	long i = 0, j = length - 1;
+	int p; // опорный элемент
+	long b = 0, e = length-1; //начало и конец массива(включительно)
+	iterators.push(b);
+	iterators.push(e);
+	for (int partLen = length; partLen > 0; partLen /= 2) // дроблю массив на подмассивы
+	{
+		
+		int count = length / partLen; //count = [1, 2, ..., length]
+		//#pragma omp parallel for shared(narr, odd) firstprivate(i, j, part, partLen)
 		for (int part = 0; part < count; part++)
 		{
-			p = partLen / 2 + part*partLen;
+			// достаю из очереди начало и конец массива
+			b = i = iterators.front(); iterators.pop();
+			e = j = iterators.front(); iterators.pop();
+			if (b >= e)
+				continue;
+			p = narr[(e + b + 1) / 2]; // выбираю опорный элемент
+
+			//вывожу массив
+			for (int _i=b; _i <= e; _i++)
+				std::cout << narr[_i] << " ";
+			std::cout << std::endl;
+			std::cout << "i = " << i << " j = " << j << std::endl;
+			std::cout <<"p = " << p << std::endl;
+
+			//стандартный алгоритм
 			do {
 				while (narr[i] < p) i++;
 				while (narr[j] > p) j--;
 
 				if (i <= j) {
+					std::cout << narr[i] << "<->" << narr[j] << std::endl;
 					Swap(narr[i], narr[j]);
+					for (int _i = b; _i <= e; _i++)
+						std::cout << narr[_i] << " ";
+					std::cout << std::endl;
 					i++; j--;
 				}
-			} while (i <= j);
+			} while (i < j);
+
+
+			if (j > b) // если j не достигнула начала
+			{
+				iterators.push(b); // i = b
+				std::cout << "(" << b << " : ";
+				auto t = e - j; // избыток, чтобы не уйти за границы массива
+				if (t < 0)
+				{
+					iterators.push(j); // j = j
+					std::cout << j + t << ")";
+				}
+				else
+				{
+					iterators.push(j);
+					std::cout << j << ")";
+				}
+
+			}
+			else // иначе создаю пустой интервалл
+			{
+				std::cout << "(" << b << " : ";
+				std::cout << b - 1 << ")";
+				iterators.push(b);
+				iterators.push(b-1);
+			}
+
+			if (e >= i) //если i не достигла конца
+			{
+				//возможно прибавлять b это очень лишнее действие
+				auto t = e - (b + i + 1);
+				if (t < 0)
+				{
+					iterators.push(i +1 + t);
+					std::cout << "(" << b + i +1 + t << " : ";
+				}
+				else
+				{
+					iterators.push(b + i +1);
+					std::cout << "(" << b + i +1 << " : ";
+				}
+				iterators.push(e);
+				std::cout << e << ")\n";
+			}
+			else
+			{
+				std::cout << "(" << b+i << " : ";
+				std::cout << b+i - 1 << ")\n";
+				iterators.push(i);
+				iterators.push(i-1);
+			}
 		}
+		std::cout << std::endl;
 	}
-	// рекурсивные вызовы, если есть, что сортировать 
-	//if (j > 0) quickSortR(arr, j);
-	//T* pointer = &arr[i];
-	//if (length > i) quickSortR(pointer, length - i);
 	return narr;
 }
